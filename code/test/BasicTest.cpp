d13f60a013d83308ac986b644214060a0fe1db27
shrek1994 <maciej.wozny41@gmail.com>
2017-01-02 23:27:17 +0100

    rerun next basic test

--- code/test/BasicTest.cpp
+++ code/test/BasicTest.cpp
@@ -9,15 +9,16 @@ const std::vector<Param> params = {
     Param{"VAR a b c BEGIN SKIP; END\n", emptyIn, expectEmptyOut, "shouldCompileCorrectlyCodeWithManyDeclaration"},
     Param{"VAR a[10] BEGIN SKIP; END\n", emptyIn, expectEmptyOut, "shouldCompileCorrectlyCodeWithOnlyOneTabDeclaration"},
     Param{"VAR a[10] b[5] c[1] BEGIN SKIP; END\n", emptyIn, expectEmptyOut, "shouldCompileCorrectlyCodeWithManyTabDeclaration"},
-    Param{"VAR a b BEGIN \n"
-          "a := b ;\n"
-          "a := a + b ;\n"
-          "a := a - b ;\n"
-          "a := a * b ;\n"
-          "a := a / b ;\n"
-          "a := a % b ;\n"
-          "END\n",
-          emptyIn, expectEmptyOut, "shouldCompileCorrectlyCodeWithAllExpressions"},
+// TODO DISABLED_
+//    Param{"VAR a b BEGIN \n"
+//          "a := b ;\n"
+//          "a := a + b ;\n"
+//          "a := a - b ;\n"
+//          "a := a * b ;\n"
+//          "a := a / b ;\n"
+//          "a := a % b ;\n"
+//          "END\n",
+//          emptyIn, expectEmptyOut, "shouldCompileCorrectlyCodeWithAllExpressions"},
     Param{"VAR a b BEGIN \
            IF a = b THEN SKIP; ELSE SKIP; ENDIF\
            IF a <> b THEN SKIP; ELSE SKIP; ENDIF\
@@ -27,25 +28,25 @@ const std::vector<Param> params = {
            IF a >= b THEN SKIP; ELSE SKIP; ENDIF\
            END\n",
           emptyIn, expectEmptyOut, "shouldCompileCorrectlyCodeWithAllConditions"},
-    Param{"VAR a b BEGIN\n"
-          "WHILE a < b DO SKIP; ENDWHILE\n"
-          "END\n",
-          emptyIn, expectEmptyOut, "shouldCompileWhileCorrectly"},
-    Param{"VAR a b BEGIN\n"
-          "FOR i FROM 1 TO 10 DO SKIP; ENDFOR\n"
-          "END\n",
-          emptyIn, expectEmptyOut, "shouldCompileForToCorrectly"},
-    Param{"VAR a b BEGIN\n"
-          "FOR i FROM 10 DOWNTO 1 DO SKIP; ENDFOR\n"
-          "END\n",
-          emptyIn, expectEmptyOut, "shouldCompileForDownToCorrectly"},
+// TODO DISABLED_
+//    Param{"VAR a b BEGIN\n"
+//          "WHILE a < b DO SKIP; ENDWHILE\n"
+//          "END\n",
+//          emptyIn, expectEmptyOut, "shouldCompileWhileCorrectly"},
+//    Param{"VAR a b BEGIN\n"
+//          "FOR i FROM 1 TO 10 DO SKIP; ENDFOR\n"
+//          "END\n",
+//          emptyIn, expectEmptyOut, "shouldCompileForToCorrectly"},
+//    Param{"VAR a b BEGIN\n"
+//          "FOR i FROM 10 DOWNTO 1 DO SKIP; ENDFOR\n"
+//          "END\n",
+//          emptyIn, expectEmptyOut, "shouldCompileForDownToCorrectly"},
 };
 } // namespace
 
 class BasicTest : public CompilerParamTest {};
 
-//TODO
-TEST_P(BasicTest, DISABLED_shouldCorrectCompile)
+TEST_P(BasicTest, shouldCorrectCompile)
 {
     run_test();
 }
