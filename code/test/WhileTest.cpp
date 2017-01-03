d88e946867fe079d32e462a93074056be48104c0
shrek1994 <maciej.wozny41@gmail.com>
2017-01-03 19:30:21 +0100

    added all 'while' tests

--- code/test/WhileTest.cpp
+++ code/test/WhileTest.cpp
@@ -5,9 +5,7 @@ namespace {
 const std::string emptyIn = "";
 
 const std::vector<Param> params = {
-        Param{"VAR a b BEGIN "
-              "a := 1;"
-              "b := 5;"
+        Param{"VAR a b BEGIN a := 1; b := 5; "
               "WHILE a < b DO "
               "WRITE a; "
               "a := a + 1; "
@@ -16,6 +14,51 @@ const std::vector<Param> params = {
               emptyIn,
               "> 1\n> 2\n> 3\n> 4\n",
               "shouldCorrectPerformWhile_lower"},
+        Param{"VAR a b BEGIN a := 1; b := 5; "
+              "WHILE a <= b DO "
+              "WRITE a; "
+              "a := a + 1; "
+              "ENDWHILE"
+              " END\n",
+              emptyIn,
+              "> 1\n> 2\n> 3\n> 4\n> 5\n",
+              "shouldCorrectPerformWhile_lowerOrEq"},
+        Param{"VAR a b BEGIN a := 5; b := 1; "
+              "WHILE a > b DO "
+              "WRITE a; "
+              "a := a - 1; "
+              "ENDWHILE"
+              " END\n",
+              emptyIn,
+              "> 5\n> 4\n> 3\n> 2\n",
+              "shouldCorrectPerformWhile_bigger"},
+        Param{"VAR a b BEGIN a := 5; b := 1; "
+              "WHILE a >= b DO "
+              "WRITE a; "
+              "a := a - 1; "
+              "ENDWHILE"
+              " END\n",
+              emptyIn,
+              "> 5\n> 4\n> 3\n> 2\n> 1\n",
+              "shouldCorrectPerformWhile_biggerOrEq"},
+        Param{"VAR a b BEGIN a := 5; b := 1; "
+              "WHILE a <> b DO "
+              "WRITE a; "
+              "a := a - 1; "
+              "ENDWHILE"
+              " END\n",
+              emptyIn,
+              "> 5\n> 4\n> 3\n> 2\n",
+              "shouldCorrectPerformWhile_NotEq"},
+        Param{"VAR a b BEGIN a := 5; b := 5; "
+              "WHILE a = b DO "
+              "WRITE a; "
+              "a := a - 1; "
+              "ENDWHILE"
+              " END\n",
+              emptyIn,
+              "> 5\n",
+              "shouldCorrectPerformWhile_NotEq"},
 };
 
 } // namespace
